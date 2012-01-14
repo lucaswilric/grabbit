6521a28b8a24fe9dcf643a012de83b510399037a
Lucas Wilson-Richter <da.maestro@gmail.com>
2012-01-14 21:04:07 +1100

    Fixed misplaced function definitions in client script

--- client/grabbit_client.rb
+++ client/grabbit_client.rb
@@ -18,59 +18,68 @@ jobs = DownloadJobFetcher.new().get_download_jobs
 
 puts "#{ jobs.length } item(s)."
 
+def get_destination_for(job, subscription)
+  dir_path = get_path_for job, subscription
+
+  file_name = DownloadJobHelper.file_name(job, subscription)
+  
+  destination = "#{ dir_path }/#{ file_name }"
+end
+
+def get_path_for(job, subscription)
+  # Copy the contents of DestinationRoot to a new instance, so we don't mutate the original.
+  dir_path = String.new(DestinationRoot)
+   
+  dir_path << "/" + DownloadJobHelper.directory(job, subscription)
+            
+  dir_path = File.expand_path(dir_path.chomp('/'))
+
+  unless dir_path.match /\A#{ DestinationRoot.chomp('/') }/
+    raise "Download #{ job.id } tried to save outside '#{ DestinationRoot }', in #{ dir_path }"
+  end
+    
+  make_directory dir_path unless File.directory?(dir_path)
+
+  dir_path
+end
+  
+def make_directory(dir_path)
+  puts "Making directory '#{dir_path}'." 
+    
+  parent_path = dir_path[0..dir_path.rindex('/')].chomp('/')
+  make_directory(parent_path) unless File.directory?(parent_path)
+    
+  Dir.mkdir(dir_path, 0777)
+end
+
+def add_to_transmission(job, subscription, dir)
+  make_directory "#{DestinationRoot}/#{dir}"
+
+  r = TransmissionHelper.new(TransmissionConfig).add_torrent_url(job['url'], "#{DestinationRoot}/#{dir}")
+  
+  puts "Added torrent '#{r['name']}'."
+end
+
+def save_file(job, subscription, dir)
+  destination = get_destination_for job, subscription
+  
+  open(destination, "wb") do |file|
+    file.write( fetch(job['url']).body )
+  end
+
+  file_name = DownloadJobHelper.file_name(job, subscription)
+
+  puts "Downloaded #{job['url']} to #{dir}."
+end
+
 DownloadJobRunner.new().run_jobs(jobs) do |job, subscription|
-#  destination = get_destination_for job, subscription
-#  
-#  open(destination, "wb") do |file|
-#    file.write( fetch(job['url']).body )
-#  end
-#
   dir = DownloadJobHelper.directory(job, subscription)
-#  file_name = DownloadJobHelper.file_name(job, subscription)
-#
-#  puts "Downloaded #{job['url']} to #{dir}."
-#  
+
   # If it's a torrent, try to add it to Transmission
   if subscription['extension'] == 'torrent'
-    make_directory "#{DestinationRoot}/#{dir}"
-
-    r = TransmissionHelper.new(TransmissionConfig).add_torrent_url(job['url'], "#{DestinationRoot}/#{dir}")
-    
-    puts "Added torrent '#{r['name']}'."
+    add_to_transmission job, subscription, dir
+  else
+    save_file job, subscription, dir
   end  
-
-  def get_destination_for(job, subscription)
-    dir_path = get_path_for job, subscription
-
-    file_name = DownloadJobHelper.file_name(job, subscription)
-    
-    destination = "#{ dir_path }/#{ file_name }"
-  end
-
-  def get_path_for(job, subscription)
-    # Copy the contents of DestinationRoot to a new instance, so we don't mutate the original.
-    dir_path = String.new(DestinationRoot)
-     
-    dir_path << "/" + DownloadJobHelper.directory(job, subscription)
-            
-    dir_path = File.expand_path(dir_path.chomp('/'))
-
-    unless dir_path.match /\A#{ DestinationRoot.chomp('/') }/
-      raise "Download #{ job.id } tried to save outside '#{ DestinationRoot }', in #{ dir_path }"
-    end
-    
-    make_directory dir_path unless File.directory?(dir_path)
-
-    dir_path
-  end
-  
-  def make_directory(dir_path)
-    puts "Making directory '#{dir_path}'." 
-    
-    parent_path = dir_path[0..dir_path.rindex('/')].chomp('/')
-    make_directory(parent_path) unless File.directory?(parent_path)
-    
-    Dir.mkdir(dir_path, 0777)
-  end
 end
 
