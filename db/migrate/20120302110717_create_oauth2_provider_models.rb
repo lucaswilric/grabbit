e4497a26811775f539f488a04edd07a3119d2780
Lucas Wilson-Richter <da.maestro@gmail.com>
2012-09-09 21:43:00 +1000

    Remove OAuth gem-dependent stuff from DB migrations

--- db/migrate/20120302110717_create_oauth2_provider_models.rb
+++ db/migrate/20120302110717_create_oauth2_provider_models.rb
@@ -1,9 +1,9 @@
 class CreateOauth2ProviderModels < ActiveRecord::Migration
   def up
-    OAuth2::Model::Schema.up
+#    OAuth2::Model::Schema.up
   end
 
   def down
-    OAuth2::Model::Schema.down
+#    OAuth2::Model::Schema.down
   end
 end
--- db/migrate/20120805081126_destroy_oauth_provider_tables.rb
+++ db/migrate/20120805081126_destroy_oauth_provider_tables.rb
@@ -1,9 +1,9 @@
 class DestroyOauthProviderTables < ActiveRecord::Migration
   def up
-    OAuth2::Model::Schema.down
+#    OAuth2::Model::Schema.down
   end
 
   def down
-    OAuth2::Model::Schema.up
+#    OAuth2::Model::Schema.up
   end
 end
